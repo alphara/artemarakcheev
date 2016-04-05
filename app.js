44f3c656db9490fa8c9a506c6a22cc89662173e7
alphara <aaar@mail.ru>
2016-04-06 00:16:22 +0800

    first version of node application

--- app.js
+++ app.js
@@ -1,43 +1,37 @@
-const http         = require('http'),
-      fs           = require('fs'),
-      path         = require('path'),
-      contentTypes = require('./utils/content-types'),
-      sysInfo      = require('./utils/sys-info'),
-      env          = process.env;
+const express = require('express');
+const bodyParser = require('body-parser')
+const fs = require('fs');
 
-let server = http.createServer(function (req, res) {
-  let url = req.url;
-  if (url == '/') {
-    url += 'index.html';
-  }
+// --- Express --- //
 
-  // IMPORTANT: Your application HAS to respond to GET /health with status 200
-  //            for OpenShift health monitoring
+var app = express();
+var env = process.env;
 
-  if (url == '/health') {
-    res.writeHead(200);
-    res.end();
-  } else if (url.indexOf('/info/') == 0) {
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Cache-Control', 'no-cache, no-store');
-    res.end(JSON.stringify(sysInfo[url.slice(6)]()));
-  } else {
-    fs.readFile('./static' + url, function (err, data) {
-      if (err) {
-        res.writeHead(404);
-        res.end();
-      } else {
-        let ext = path.extname(url).slice(1);
-        res.setHeader('Content-Type', contentTypes[ext]);
-        if (ext === 'html') {
-          res.setHeader('Cache-Control', 'no-cache, no-store');
-        }
-        res.end(data);
-      }
-    });
-  }
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+app.use(bodyParser.json());         // to support JSON-encoded bodies
+
+app.use(express.static(__dirname + '/public'));
+
+// --- Routes --- //
+
+// IMPORTANT: Your application HAS to respond to GET /health with status 200
+//            for OpenShift health monitoring
+app.get('/health', (req, res) => {
+  res.writeHead(200);
+  res.end();
 });
 
-server.listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost', function () {
+app.get('*', function(req, res) {
+  fs.readFile(__dirname + '/index.html', 'utf8', function(err, text) {
+    res.send(text);
+  });
+});
+
+// --- Web server --- //
+
+app.listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost', function () {
   console.log(`Application worker ${process.pid} started...`);
 });
+
