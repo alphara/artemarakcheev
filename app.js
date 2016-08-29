6c8ae01df02258e039c638d4342a2b979518aec9
alphara <aaar@mail.ru>
2016-08-29 21:43:28 +0300

    photo, update skills in resume

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const fs = require('fs');
+const morgan = require('morgan');
 
 // --- Express --- //
 
 var app = express();
 var env = process.env;
 
+app.use(morgan('combined'));
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }));
 app.use(bodyParser.json());         // to support JSON-encoded bodies
 
-app.use(express.static(__dirname + '/public'));
+// app.use(express.static('public'));
+app.use('/public', express.static(__dirname + '/public'));
 
 // --- Routes --- //
 
@@ -21,6 +24,18 @@ app.use(express.static(__dirname + '/public'));
 app.get('/health', (req, res) => {
   res.writeHead(200);
   res.end();
+});
+
+app.get('/hyposis', function(req, res) {
+  fs.readFile(__dirname + '/hypnosis.html', 'utf8', function(err, text) {
+    res.send(text);
+  });
+});
+
+app.get('/unicorn', function(req, res) {
+  fs.readFile(__dirname + '/unicorn.html', 'utf8', function(err, text) {
+    res.send(text);
+  });
 });
 
 app.get('*', function(req, res) {
